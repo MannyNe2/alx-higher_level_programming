0403a0be5b19a6dc165e99f2e7f29ba07ad114c4
Manny Negussie <102585213+MannyNe2@users.noreply.github.com>
2022-07-19 22:08:21 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,57 +1,55 @@
 #!/usr/bin/python3
-"""
-No module imported
+""" Creates an empty class called Square
 """
 
 
 class Square:
-    """
-    Private instance attribute size
-    public instance method
+    """ Empty class with size private attribute
     """
     def __init__(self, size=0):
-        """private instance attribute
-        parameters
-        -------------------------
-        size : integer else TypeError
-        if size less than 0, raise value error
         """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
         """
-        to retrieve private instance attribute
+        size getter. Handle size errors
         """
         return self.__size
 
     @size.setter
     def size(self, value):
         """
-        to set private instance attribute
+        size setter. Set the size square
         """
-        self.__size = value
-        try:
-            assert type(value) == int
-        except:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-
-    def area(self):
-        """
-        public instance method
-        returns the current square area
-        """
-        return self.__size ** 2
+        self.__size = value
 
     def my_print(self):
         """
-        print squre using #
+        Print a square with the character #
         """
         if self.__size == 0:
             print()
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
-            print()
+        else:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
